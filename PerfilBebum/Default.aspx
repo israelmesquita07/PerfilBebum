75740095d4fbe816aced8e609e8b0e165fbf9477
Israel Mesquita <israelmesquita07@outlook.com>
2018-04-06 22:24:37 -0300

    Initial Commit

---
+++ PerfilBebum/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+      <link rel="stylesheet" href="../Content/css/style.css" />
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container">
+            <form id="form1" runat="server" class="form">
+                <div>
+                    <h1>Perfil do Bebum</h1>
+                    <asp:Label ID="lblBebidas" runat="server" Text="Bebidas"></asp:Label>
+                    <asp:DropDownList ID="ddlBebidas" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlBebidas_SelectedIndexChanged">
+                    </asp:DropDownList>
+                    <asp:Label ID="lblDoses" runat="server" Text="Doses"></asp:Label>
+                    <asp:DropDownList ID="ddlDoses" runat="server">
+                    </asp:DropDownList>
+                    <asp:Button ID="btnGerarPerfil" runat="server" Text="GERAR PERFIL" OnClick="btnGerarPerfil_Click" />
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ PerfilBebum/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    WebService.WebServiceSoapClient cliente = new WebService.WebServiceSoapClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            ddlBebidas.DataSource = cliente.Bebidas();
+            ddlBebidas.DataValueField = "Id";
+            ddlBebidas.DataTextField = "Descricao";
+            ddlBebidas.DataBind();
+            ddlBebidas.Items.Insert(0, new ListItem("Selecione", "0")); //Além das Bebidas, insere-se uma opção "Selecione" para ficar mais intuitivo.
+        }
+    }
+
+    protected void ddlBebidas_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var valorSelecionado = ddlBebidas.SelectedValue;
+        if (!String.IsNullOrEmpty(valorSelecionado) && valorSelecionado != "0") //Verifica se o valor selecionado é válido.
+        {
+            ddlDoses.DataSource = cliente.Doses(int.Parse(valorSelecionado));
+            ddlDoses.DataValueField = "Id";
+            ddlDoses.DataTextField = "QuantidadeDose";
+            ddlDoses.DataBind();
+        }
+        else
+        {
+            ddlDoses.DataSource = "";
+            ddlDoses.DataBind();
+        }
+
+    }
+
+    protected void btnGerarPerfil_Click(object sender, EventArgs e)
+    {
+        var id_Doses = ddlDoses.SelectedValue;
+        if (!String.IsNullOrEmpty(id_Doses))
+        {
+            List<string> perfil = cliente.Perfil(int.Parse(id_Doses)).ToList(); //Retorna o perfil segundo o que foi informado no formulário.
+            Response.Write("<script>alert('O seu Perfil é de um " + perfil[0] + " " + perfil[1] + "');</script>");
+        }
+        else
+        {
+            Response.Write("<script>alert('Selecione opções válidas');</script>"); //Caso o valor selecionado seja inválido, retorna esta mensagem para o usuário.
+        }
+    }
+}
